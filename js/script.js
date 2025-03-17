0012fdd11685a53558386cd2ac7a53deaaf6abf1
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-17 15:57:11 +0100

    [ADD] create initial HTML structure and JavaScript for train ticket calculation

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//chiedere all'utente il numero di chilometri che desidera percorrere
+//dichiarare le variabili
+let chilometri = parseInt(prompt('Inserisci il numero di chilometri che desideri percorrere'));
+//chiedere l'età del passeggero
+let etaPasseggero = parseInt(prompt('Inserisci l\'età del passeggero'));
+//calcolare il prezzo base
+let prezzoBase;
+let sconto;;
+
+
+
+//calcolare lo sconto
+
+
+
+
+
+
+//calcolare il prezzo finale
